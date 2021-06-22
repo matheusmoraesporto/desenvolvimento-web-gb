5bfccfd92fb480f1593f6b54bd54ff57738d3d62
Jennifer Diehl <jenniferdiehll@hotmail.com>
2021-06-22 20:15:45 -0300

    Tela de carrinho vazio

--- client/src/pages/empty-cart/empty-cart.js
+++ client/src/pages/empty-cart/empty-cart.js
@@ -1,13 +1,18 @@
 import React from 'react';
-
+import MarioEmptyCart from '../../assets/backgrounds/emptycart.jpg'
+import './styles.css'
 
 export function EmptyCart(){
 
     return(
-        <div>
+        <div className="empty-cart-container">
+        <span class="spn-empty-cart">O carrinho está vazio! Volte a tela inicial e adicione um produto.</span>
+        
+        <img src={MarioEmptyCart} alt="" />
 
-        desenvolvimento da tela aqui
-
+        <button type="submit" id="continue-buying" class="cursor-pointer btn">
+        Continue comprando
+        </button>
         </div>
     );
 }
